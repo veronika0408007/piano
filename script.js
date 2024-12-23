f0af35b4eeaeca2da1602ae7c17e874f2b05a756
veronika0408007 <veronika.jarig@inbox.lv>
2024-12-23 22:27:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ function toggleNotes() {
     document.querySelector('.piano').classList.toggle('show-notes');
 }
 
-const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('click', playNote));
+document.querySelectorAll('.key').forEach(key => {
+    key.addEventListener('click', playNote);
+});
 
 function playNote() {
     const note = this.dataset.note;
